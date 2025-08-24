b657304ce7e5a2f7b45526f9ae82288da73c0373
GabiDragh <draghici.gabi89@gmail.com>
2025-08-24 16:07:16 +0100

    Default camera position log set

--- components/Aquarium.tsx
+++ components/Aquarium.tsx
@@ -6,12 +6,26 @@ import Building from "../components/Building"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Water from "../components/Water";
 
+// INFO: Camera poition log -> use only when needed otherwise it flods the log
+// function CameraDebugger() {
+//     const {camera } = useThree()
+   
+//     useFrame(() => {
+//         console.log("Position:", camera. position)
+//         console.log("Rotation:", camera.rotation)
+        
+//     })
+//     return null
+// }
+
+
 export default function Aquarium() {
   return (
     <div className="w-screen h-screen">
-      <Canvas camera={{ position: [5, 4, 12], fov: 45 }}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
+      <Canvas camera={{ position: [-17.51, 2.86, 21.14], fov: 50 }}>
+        {/* <CameraDebugger /> */}
+        <ambientLight intensity={1} />
+        <directionalLight position={[10, 10, 5]} intensity={0.7} />
         <Environment preset="sunset" />
         <OrbitControls />
 
