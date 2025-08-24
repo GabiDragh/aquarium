ea5a97c41b99da2745417fb7bc7f9bda332c0697
GabiDragh <draghici.gabi89@gmail.com>
2025-08-24 16:47:07 +0100

    Camera position update

--- components/Aquarium.tsx
+++ components/Aquarium.tsx
@@ -22,7 +22,7 @@ import Water from "../components/Water";
 export default function Aquarium() {
   return (
     <div className="w-screen h-screen">
-      <Canvas camera={{ position: [-17.51, 2.86, 21.14], fov: 50 }}>
+      <Canvas camera={{ position: [-17.51, 2.86, 20.14], fov: 50 }}>
         {/* <CameraDebugger /> */}
         <ambientLight intensity={1} />
         <directionalLight position={[10, 10, 5]} intensity={0.7} />
