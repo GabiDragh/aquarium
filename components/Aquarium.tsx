dbb13cb58e704c604e9520a5da8a407c7e7531b1
GabiDragh <draghici.gabi89@gmail.com>
2025-08-24 21:51:11 +0100

    Notes

--- components/Aquarium.tsx
+++ components/Aquarium.tsx
@@ -17,22 +17,27 @@ import Water from "../components/Water";
 //     return null
 // }
 
+// TODO: Add Leva collapse={false} to each folder
 
 export default function Aquarium() {
   return (
     <div className="w-screen h-screen">
       <Canvas camera={{ position: [-17.51, 2.86, 20.14], fov: 50 }}>
         {/* <CameraDebugger /> */}
+
         <ambientLight intensity={1} />
         <directionalLight position={[10, 10, 5]} intensity={0.7} />
+
         <Environment preset="forest" />
         <OrbitControls />
+
         <AccumulativeShadows temporal frames={100} colorBlend={0.5} opacity={0.6} scale={25}>
             <RandomizedLight radius={8} ambient={0.3} intensity={1.5} position={[5, 10, -5]} />
         </AccumulativeShadows>
 
         <Building />
         <Water />
+
       </Canvas>
     </div>
   );
